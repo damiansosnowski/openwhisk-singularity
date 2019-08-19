db0ebbcce1b6d2c0e3c6a2bd9b4351bac6a9c27e
Damian Sosnowski <sosnowski.damian@gmail.com>
2019-08-20 01:11:56 +0200

    some changes

--- local-invoker-deployment/init.sh
+++ local-invoker-deployment/init.sh
@@ -3,7 +3,7 @@
 # Licensed to the Apache Software Foundation (ASF) under one or more contributor
 # license agreements; and to You under the Apache License, Version 2.0.
 
-export INVOKER_OPTS
-INVOKER_OPTS="$INVOKER_OPTS $(./transformEnvironment.sh)"
+source environment-variables.sh
+export INVOKER_OPTS="$INVOKER_OPTS $(./transformEnvironment.sh)"
 
-exec invoker/bin/invoker "$@"
+./invoker/bin/invoker --id 0 --uniqueName 0 #>> ./logs/invoker0_logs.log 2>&1
